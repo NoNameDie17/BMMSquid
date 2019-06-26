c7dd6845f0d19bfa4b004222ddff20da7c41af96
NoNameDie17 <52222327+NoNameDie17@users.noreply.github.com>
2019-06-27 06:38:21 +0700

    Add files via upload

---
+++ NoNameDie.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+# BY   : NoNimeDie
+# TEAM : Black Hat 
+# pesan:Jangan di recordya Tod
+
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+clear
+
+
+figlet NoNaMeDie | lolcat
+
+
+echo " ╔═══════════════════════════════════════╗" | lolcat
+echo " |          🎩By Mr.NoNaмε_Diε🎩         |" | lolcat
+echo " ╚═══════════════════════════════════════╝" | lolcat
+echo " |             Tools InsTaller           |" | lolcat    
+echo " |         Kontak me: 085770617543       |" | lolcat
+echo " |       TEAM GOMBONG ERROR SYSTEAM      |" | lolcat
+echo " |      username tool 1: R133FGANZ00987  |"
+echo " |           pasword: R133F9820019       |"
+echo " ╚═══════════════════════════════════════╝" |lolcat
+echo
+
+
+
+
+echo
+echo       "==≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈======" | lolcat
+echo        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" | lolcat 
+echo $green "   [1].Attak No Hp_Trojans     |"
+echo        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" |lolcat
+echo $green "   {2}.insttal Tool BAJINGAN   |"  
+echo        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" | lolcat
+echo $green "   {3}.Dark facebook           |" 
+echo        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" | lolcat
+echo $green "   [4].insttal tool TUANB4DUT  |" 
+echo        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" | lolcat
+echo $green "   [5].hack cctv Random        |"
+echo        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" | lolcat
+echo $green "   [6].sms_brutal              |"
+echo        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" | lolcat
+echo $green "   [7].Lacak_Ip                |"
+echo        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" | lolcat
+echo $green "   [8].install Metasploit      |" 
+echo        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" | lolcat
+echo        "    [9].Exit                    |" | lolcat
+echo        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" | lolcat
+echo       "=======================================" | lolcat 
+echo ¥¥¥"╭─Mr.NoName_Die" |lolcat
+  read -p ¥¥¥"╰─#" pill 
+if [ $pill = 1 ]
+then
+clear
+echo $blue "Sabar Zheyeng Lagi Load" 
+echo
+sleep 1
+pkg install git
+git clone https://github.com/R133F/Death-tro
+cd Death-tro
+python2 death-trojan.py
+
+
+fi
+echo $blue "Thank Zheyeng" | lolcat
+echo 
+if [ $pill = 2 ]
+then
+clear
+sleel 1
+pkg install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6.git 
+cd BAJINGANv6 
+sh BAJINGAN.sh 
+fi
+
+
+
+if [ $pill = 3 ]
+then
+clear
+sleel 1
+pkg install git
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+
+
+if [ $pill = 4 ]
+then
+clear
+sleer 1
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+chmod 777 TUANB4DUT..sh
+./TUANB4DUT..sh
+fi
+
+
+
+if [ $pill = 5 ]
+then
+clear
+sleer 1
+pkg install git
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+
+
+if [ $pill = 6 ]
+then
+clear
+sleer 1
+pkg install git
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+echo $blue "sukes"
+echo
+python2 run.py
+fi
+
+
+
+if [ $pill = 7 ]
+then
+clear
+sleer 1
+pkg install git
+git clone https://github.com/iexei/scanip
+cd scanip
+python2 scan.py
+fi
+
+
+if [ $pill = 8 ]
+then
+figlet Install | lolcat
+figlet Metasploit | lolcat
+echo $green"******************************************************************"
+echo " proses penginstalannya cukup memakai waktu lama tod jadi harap sabar ya"
+echo
+echo " apakah Anda Ingin Lanjut [Y/N]"
+read Y
+echo
+echo " Tunggu ya.."
+echo "Loading"
+clear
+sleer 1
+pkg install git
+rm -rf /var/lib/dpkg/lock
+dpkg --configure -a
+apt-get update
+pkg install metasploit
+msfconsole
+fi
+
+if [ $pill = 9 ]
+then
+clear
+figlet Thank ya🙏 | lolcat
+sleep 1
+exit
+fi
+
+
